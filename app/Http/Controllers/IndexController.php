3988e1624ffa054acdeac7cb0c3ec00129d40127
Ubaid Ur Rehman <tahaubaid27@gmail.com>
2024-08-21 18:05:56 +0500

    Actiate navbar and Footer buttons

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,10 +7,30 @@ use App\Models\Client_contact;
 
 class IndexController extends Controller
 {
-    public function index(Request $request)
+    private function getClientAndContact()
     {
         $client = session('active_client');
         $contact = Client_contact::where('client_id', $client->id)->first();
-        return view('index', compact('client', 'contact'));
+        return compact('client', 'contact');
+    }
+
+    public function index(Request $request)
+    {
+        $data = $this->getClientAndContact();
+        return view('index', $data);
+    }
+
+    public function service(Request $request)
+    {
+        $data = $this->getClientAndContact();
+        return view('service', $data);
+    }
+
+    public function contact(Request $request)
+    {
+        $data = $this->getClientAndContact();
+        return view('contact', $data);
     }
 }
+
+
