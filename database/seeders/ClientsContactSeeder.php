594a5ceca6f039fa4a362ba608c24dcfc56689e8
Ubaid Ur Rehman <tahaubaid27@gmail.com>
2024-09-03 15:15:35 +0500

    4chainslogistics contact info

--- database/seeders/ClientsContactSeeder.php
+++ database/seeders/ClientsContactSeeder.php
@@ -32,5 +32,13 @@ class ClientsContactSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        DB::table('client_contacts')->insert([
+            'phone_number' => '469-694-0707',
+            'email' => 'skywaysexpresdispatch@gmail.com',
+            'address' => '7901 4TH ST N STE 300 ST PETERSBURG FL 33702',
+            'client_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
